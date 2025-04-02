953061eae28f6773b7422bf61daed0f9e2efc6a2
qobilovfirdavs02 <qobilovfirdavs2002@gmail.com>
2025-04-02 12:25:52 +0300

    adding dockerfile

--- main.py
+++ main.py
@@ -29,4 +29,5 @@ PORT = int(os.getenv("PORT", 8000))  # Railway PORT ni olamiz yoki 8000 default
 HOST = "0.0.0.0"
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import uvicorn
+    uvicorn.run("main:app", host=HOST, port=PORT, reload=True)
